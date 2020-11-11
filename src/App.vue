cfeed80a2e912de6dc46518fec863205729178ce
mauriciolobo <mauriciolobo@gmail.com>
2020-11-11 20:04:05 -0300

    Changed

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,6 @@
 <template>
   <div id="app">
-    <h1>It worked!</h1>
+    <h1>Easy to Change!</h1>
     <img alt="Vue logo" src="https://vuejs.org/images/logo.png">
     <HelloWorld msg="Welcome to Your Vue.js App"/>
   </div>
